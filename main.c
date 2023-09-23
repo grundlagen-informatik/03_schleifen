f1def39d281e29a99ed17775985053ad96f3224d
TonyMacarony <antoine.gilliand@gmail.com>
2023-09-23 10:15:50 +0200

    Aufgabe 2 gelöst

--- main.c
+++ main.c
@@ -39,16 +39,31 @@ void zahlenAusgabeAddition(){
     }
     printf("Hier sind alle ganzzahligen Zahlen von 0 bis %d: \n", eingabe);
         for (int i = 0; i <= eingabe; i++) {
-            if (i<10) {printf("%d  ", i);}
-                 else {printf("%d ", i);}
+            if (i<10) {printf("  %d ", i);}
+                 else if (i<100) {printf(" %d ", i);}
+                 else {printf("%d ",i);}
             zaehler = zaehler + i;
             if (i%20==0) {printf("\n");}
         }
-
     printf("\nHier ist die Summe der ganzzahligen Zahlen von 0 bis %d: \n", eingabe);
     printf("%d",zaehler);
 }
 
 void zahlenAusgabe(){
     // Hier muss der Code für die Zahlen-Ausgabe geschrieben werden.
+    int eingabe=1002; bool eingabecheck = false;
+    while(!eingabecheck) {
+        if (eingabe>1000) {
+            printf("Bitte wähle eine ganzzahlige Zahl <= 1000: \n");
+            scanf("%d",&eingabe);
+        } else {eingabecheck = true;}
+    }
+    printf("Hier sind alle ganzzahligen Zahlen von %d bis 0: \n", eingabe);
+    for (int k=eingabe; k>=0; k--) {
+            if (k<10) {printf("  %d ", k);}
+            else if (k<100) {printf(" %d ", k);}
+            else {printf("%d ", k);}
+            if (k%20==0) {printf("\n");}
+    }
+    printf("\nMeldung: Programm wurde korrekt ausgeführt.");
 }
